79e342877df2d62e53900b72ba2b6ef24b4e8a50
ianwremmel <1182361+ianwremmel@users.noreply.github.com>
2020-08-16 10:58:52 -0700

    ci: configure linters

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 'use strict';
 
-require('./babel-register')
+require('./babel-register');
+// as our typescript entrypoint, eslint-plugin-import doesn't know src/index
+// exists
+// eslint-disable-next-line import/no-unresolved
 require('./src/index');
